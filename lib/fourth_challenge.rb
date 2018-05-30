4f3b2baa21fbdde1fcd516f2a951192de3bac063
Paul Bomba <paul@paulbomba.com>
2018-05-30 15:35:36 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,8 +5,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {}, 
-        {}
+        {name: "", age: "", attitude: ""}, 
+        {name: "", age: "", attitude: ""}
       ]
    }, 
    :capulet => {
@@ -14,8 +14,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {},
-        {}
+        {name: "", age: "", attitude: ""},
+        {name: "", age: "", attitude: ""}
       ]
    }
 }
